8f3218df4a3b2436dc50210850148fd80d23b646
Anzh <anzhrai@gmail.com>
2020-11-16 16:07:54 +0530

    Database Corrected

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const MongoClient = require("mongodb").MongoClient;
 const ObjectID = require("mongodb").ObjectID;
 const dbname = "chat-app";
-const url = `mongodb+srv://conflux:conflux@conflux.e0fak.mongodb.net/conflux?retryWrites=true&w=majority/${dbname}`;
+const url = `mongodb+srv://conflux:conflux@conflux.e0fak.mongodb.net/${dbname}`;
 const mongoOptions = { useNewUrlParser: true };
 
 const state = {
